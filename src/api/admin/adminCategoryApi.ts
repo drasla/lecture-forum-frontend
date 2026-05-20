f097ac9ed797e12515c90e53396ae6eb47666eec
Tae.Garam (Office) <drasla217@gmail.com>
2026-05-20 16:22:56 +0900

    AdminCategoryEditPage 완성

--- src/api/admin/adminCategoryApi.ts
+++ src/api/admin/adminCategoryApi.ts
@@ -1,11 +1,17 @@
 import axiosInstance from "../axiosInstance.ts";
 import type { Category } from "../../types/category.type.ts";
 import type { AdminCreateCategoryInputType } from "../../schemas/admin/category/adminCreateCategorySchema.ts";
+import type { AdminEditCategoryInputType } from "../../schemas/admin/category/adminEditCategorySchema.ts";
 
 const fetchCategoryList = async (): Promise<Category[]> => {
     const response = await axiosInstance.get("/admin/category/list");
     return response.data.data;
 };
+
+const fetchCategoryById = async (id: number) => {
+    const response = await axiosInstance.get(`/admin/category/${id}`);
+    return response.data.data;
+}
 
 // 카테고리 등록 API
 const createCategory = async (data: AdminCreateCategoryInputType): Promise<Category> => {
@@ -14,6 +20,10 @@ const createCategory = async (data: AdminCreateCategoryInputType): Promise<Categ
 };
 
 // 카테고리 수정 API
+const updateCategory = async (id: number, data: AdminEditCategoryInputType): Promise<Category> => {
+    const response = await axiosInstance.patch(`/admin/category/${id}`, data);
+    return response.data.data;
+}
 
 // 카테고리 온오프 API
 const toggleCategoryStatus = async (id: number) => {
@@ -23,6 +33,8 @@ const toggleCategoryStatus = async (id: number) => {
 
 export default {
     fetchCategoryList,
+    fetchCategoryById,
     createCategory,
     toggleCategoryStatus,
+    updateCategory,
 };
