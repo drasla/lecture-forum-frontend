c2a95eb078002638ee0a25b5e4e4b3ca253da8db
Tae.Garam (Office) <drasla217@gmail.com>
2026-05-22 14:12:46 +0900

    AdminUserListPage 작성

--- src/router/GetRouter.tsx
+++ src/router/GetRouter.tsx
@@ -9,6 +9,7 @@ import { useAuthStore } from "../stores/auth/authStore.ts";
 import { Role } from "../types/user.type.ts";
 import AdminCategoryCreatePage from "../pages/admin/category/create/AdminCategoryCreatePage.tsx";
 import AdminCategoryEditPage from "../pages/admin/category/edit/AdminCategoryEditPage.tsx";
+import AdminUserListPage from "../pages/user/AdminUserListPage.tsx";
 
 // 회원의 권한에 따라 접근할 수 있는 주소를 판별하기 위해서
 // react-router 라이브러리에서는 "로더(loader)"라는 기능을 제공함
@@ -75,6 +76,12 @@ const router = createBrowserRouter([
                     { index: true, element: <AdminCategoryListPage /> },
                     { path: "create", element: <AdminCategoryCreatePage /> },
                     { path: "edit/:id", element: <AdminCategoryEditPage /> },
+                ],
+            },
+            {
+                path: "user",
+                children: [
+                    { index: true, element: <AdminUserListPage /> }
                 ],
             },
         ],
