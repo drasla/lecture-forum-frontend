3d0810e203898b886c363c28ca78335d7e85bd95
Tae.Garam (Office) <drasla217@gmail.com>
2026-05-22 17:18:54 +0900

    AdminUpdateUserPage 작성

--- src/pages/admin/user/AdminUserListPage.tsx
+++ src/pages/admin/user/AdminUserListPage.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import {
+    AdminButtonGroup,
     AdminContainer,
     AdminLoadingText,
     AdminPageHeader,
@@ -15,6 +16,7 @@ import Badge from "../../../components/common/badge/Badge.tsx";
 import { Link } from "react-router";
 import { Role, type User } from "../../../types/user.type.ts";
 import adminUserApi from "../../../api/admin/user/adminUserApi.ts";
+import { FiEdit } from "react-icons/fi";
 
 function AdminUserListPage() {
     const [list, setList] = useState<User[]>([]);
@@ -92,8 +94,20 @@ function AdminUserListPage() {
                                                 {item.deletedAt ? "탈퇴" : "정상"}
                                             </Badge>
                                         </AdminTd>
-                                        <AdminTd>{new Date(item.createdAt).toLocaleString()}</AdminTd>
-                                        <AdminTd>기능</AdminTd>
+                                        <AdminTd>
+                                            {new Date(item.createdAt).toLocaleString()}
+                                        </AdminTd>
+                                        <AdminTd>
+                                            <AdminButtonGroup>
+                                                <Button
+                                                    variant={"icon"}
+                                                    color={"primary"}
+                                                    as={Link}
+                                                    to={`/admin/user/${item.id}`}>
+                                                    <FiEdit size={18} />
+                                                </Button>
+                                            </AdminButtonGroup>
+                                        </AdminTd>
                                     </tr>
                                 ))}
                             </tbody>
