b5debe00cd75722f65e1f72af8839d63dc0d3c7d
Tae.Garam (Office) <drasla217@gmail.com>
2026-06-01 12:51:11 +0900

    PostDetailPage 투표 기능 작성

--- src/api/user/postApi.ts
+++ src/api/user/postApi.ts
@@ -3,7 +3,11 @@ import type { PaginationResponseType } from "../../types/common.type.ts";
 import type { Post } from "../../types/post.type.ts";
 import type { CreatePostInputType } from "../../schemas/post/createPostSchema.ts";
 
-const fetchPostListByCategory = async (categoryId: number, page: number, size: number): Promise<PaginationResponseType<Post>> => {
+const fetchPostListByCategory = async (
+    categoryId: number,
+    page: number,
+    size: number,
+): Promise<PaginationResponseType<Post>> => {
     const response = await axiosInstance.get(`/post/list/${categoryId}?page=${page}&size=${size}`);
     return response.data.data;
 };
@@ -11,15 +15,21 @@ const fetchPostListByCategory = async (categoryId: number, page: number, size: n
 const fetchPostById = async (id: number): Promise<Post> => {
     const response = await axiosInstance.get(`/post/${id}`);
     return response.data.data;
-}
+};
 
 const createPost = async (data: CreatePostInputType) => {
     const response = await axiosInstance.post("/post/create", data);
     return response.data.data;
 };
 
+const votePost = async (postId: number, option: number) => {
+    await axiosInstance.post(`/post/${postId}/vote`, { option });
+    // 백엔드가 처리 후 응답(Response)하는 내용이 필요 없으면 return 안해도 됨
+};
+
 export default {
     fetchPostListByCategory,
     createPost,
     fetchPostById,
+    votePost,
 };
